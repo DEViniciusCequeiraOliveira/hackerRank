907f3c9804b19d39edc48532cf978e431285714d
Vinicius Cerqueira Oliveira <dev.vinicius.c.oliveira@gmail.com>
2023-08-27 15:52:03 -0300

    Diagonal Difference

---
+++ Diagonal Difference/DiagonalDifference/DiagonalDifference.java
@@ -0,0 +1,62 @@
+package DiagonalDifference;
+
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'diagonalDifference' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+     */
+
+    public static int diagonalDifference(List<List<Integer>> arr) {
+        // Write your code here
+        // 00 11 22
+        // 02 11 20
+        int leftDiagonal = 0, rightDiagonal = 0;
+        for (Integer i = 0; i < arr.size(); i++) {
+            leftDiagonal += arr.get(i).get(i);
+            rightDiagonal += arr.get(i).get((arr.size() - 1) - i);
+        }
+
+        return Math.abs(leftDiagonal - rightDiagonal);
+    }
+
+}
+
+public class DiagonalDifference {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<List<Integer>> arr = new ArrayList<>();
+
+        IntStream.range(0, n).forEach(i -> {
+            try {
+                arr.add(
+                        Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                                .map(Integer::parseInt)
+                                .collect(toList())
+                );
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        int result = Result.diagonalDifference(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
